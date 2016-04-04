27fa50d9a4adc613c5def37d7785dd4764deea88
Dan Ramich <danold215@gmail.com>
2016-04-04 15:22:12 -0700

    Update to app.js

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ var express = require('express')
 var app = express();
 var path = require('path');
 
+
 var port=process.env.PORT || 8080; 
+
+app.use(express.static(__dirname + "/public"));
+
 
 // function handleRequest(request, response){
 //     response.end('It Works!! Path Hit: ' + request.url);
