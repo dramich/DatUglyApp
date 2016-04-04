4b0dbe33d2a8cf00178358429a7c090e64a662d2
Dan Ramich <danold215@gmail.com>
2016-04-04 16:36:54 -0700

    Update form

--- public/app.js
+++ public/app.js
@@ -1,22 +1,27 @@
 var app = angular.module('uglyBook', []);
 
-app.controller('uglyController', function($scope, addRecipe){
+app.controller('uglyController', function($scope, recipeFact){
   $scope.recipes = [
     {title: 'test', url: 'www.yay.com'},
     {title: 'test2', url: 'www.yay2.com'}
   ];
-    // $scope.recipes = addRecipe.data
-    $scope.addNew = addRecipe.addNew;
+    // $scope.recipes = recipeFact.data
+    $scope.addNew = recipeFact.addNew;
+    $scope.removeRecipe = recipeFact.removeRecipe
    console.log("Ugly Controller reporting for duty.");
 });
 
-app.factory('addRecipe', function(){
+app.factory('recipeFact', function(){
   var data = [{title: 'test', url: 'www.yay.com'}];
   var service = {};
   service.addNew = function(){
     this.recipes.push({title: this.title, url:this.link})
     this.title = '';
     this.link = '';
+  }
+
+  service.removeRecipe = function(recipe){
+    this.recipes.splice(this.recipes.indexOf(recipe), 1);
   }
 
   return service
