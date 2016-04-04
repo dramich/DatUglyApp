c201cbb78fc21acbf0924d095ccb339cf6db4588
Dan Ramich <danold215@gmail.com>
2016-04-04 16:22:13 -0700

    Submit link work

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 //var http = require('http');
 var express = require('express')
-var app = express();
 var path = require('path');
 
 
 var port=process.env.PORT || 8080; 
 
+var app = express();
 app.use(express.static(__dirname + "/public"));
 
 
--- public/app.js
+++ public/app.js
@@ -1,6 +1,24 @@
 var app = angular.module('uglyBook', []);
 
-app.controller('uglyController', function($scope){
-  $scope.recipes = 'Hello world';
+app.controller('uglyController', function($scope, addRecipe){
+  $scope.recipes = [
+    {title: 'test', url: 'www.yay.com'},
+    {title: 'test2', url: 'www.yay2.com'}
+  ];
+    // $scope.recipes = addRecipe.data
+    $scope.addNew = addRecipe.addNew;
    console.log("Ugly Controller reporting for duty.");
 });
+
+app.factory('addRecipe', function(){
+  var data = [{title: 'test', url: 'www.yay.com'}];
+  var service = {};
+  service.addNew = function(){
+    this.recipes.push({title: this.title, url:this.link})
+    this.title = '';
+    this.link = '';
+  }
+
+  return service
+  return data
+})
