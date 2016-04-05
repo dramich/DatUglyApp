7d3f6a6cc7f947de738477669a6b8a2702ddab96
Dan Ramich <danold215@gmail.com>
2016-04-04 20:58:38 -0700

    Update App

--- app.js
+++ app.js
@@ -1,27 +1,17 @@
 //var http = require('http');
 var express = require('express')
 var path = require('path');
-
+var db = require('./DB/config.js')
 
 var port=process.env.PORT || 8080; 
 
 var app = express();
 app.use(express.static(__dirname + "/public"));
 
-
-// function handleRequest(request, response){
-//     response.end('It Works!! Path Hit: ' + request.url);
-// }
-
-// var server = http.createServer(handleRequest);
-
-// server.listen(PORT, function(){
-//     console.log("Server listening on: http://localhost:%s", PORT);
-// });
-
 app.get('/', function(req, res){
-  console.log('stuff')
+  console.log('stuff');
   res.sendFile(path.join(__dirname + '/public/index.html'))
 });
 
 app.listen(port);
+
